b2f1443d9cb91111f702e9b9ade249e45ac8b22c
Duc Nguyen <dbackspace.hust@gmail.com>
2021-03-02 08:15:03 +0700

    default -> void (ILoader)

--- app/src/main/java/com/xlteam/givelove/external/repository/ILoader.java
+++ app/src/main/java/com/xlteam/givelove/external/repository/ILoader.java
@@ -2,7 +2,5 @@ package com.xlteam.givelove.external.repository;
 import java.util.List;
 
 public interface ILoader<CaptionType> {
-    default void loadResult(int loaderTaskType, List<CaptionType> result) {
-        return;
-    }
+    void loadResult(int loaderTaskType, List<CaptionType> result);
 }
